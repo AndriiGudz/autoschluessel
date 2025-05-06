c4ad1f4b4d8c6432225677979276f7f11e4e933e
Andrii Gudz <andrey.gudz@gmail.com>
2025-05-06 17:28:14 +0200

    update settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
+  site: 'https://mv-kfz.netlify.app',
   output: 'server',
   adapter: netlify(),
 });
